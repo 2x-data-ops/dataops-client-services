8488c2e63cabe98a9b1c3b0d0745c4b2a6874ece
aiem2x <meor.hasyim@2x.marketing>
2024-02-27 15:33:09 +0800

    2024-02-27 - Added smartcom.db_6sense_buying_stages_movement table

--- Clients/Smartcommunications/smartcommunications_6sense_impact_dashboard.sql
+++ Clients/Smartcommunications/smartcommunications_6sense_impact_dashboard.sql
@@ -1,3 +1,85 @@
+CREATE OR REPLACE TABLE `smartcom.db_6sense_buying_stages_movement` AS
+
+WITH sixsense_stage_order AS (
+        SELECT
+            'Target' AS _buying_stage,
+            1 AS _order
+        UNION ALL
+        SELECT
+            'Awareness' AS _buying_stage,
+            2 AS _order
+        UNION ALL
+        SELECT
+            'Consideration' AS _buying_stage,
+            3 AS _order
+        UNION ALL
+        SELECT
+            'Decision' AS _buying_stage,
+            4 AS _order
+        UNION ALL
+        SELECT
+            'Purchase' AS _buying_stage,
+            5 AS _order
+    ),
+    sixsense_buying_stage_data AS (
+        SELECT
+            DISTINCT ROW_NUMBER() OVER (
+                PARTITION BY _6sense_company_name,
+                _6sense_country,
+                _6sense_domain
+                ORDER BY
+                    CASE
+                        WHEN extract_date LIKE '%/%' THEN PARSE_DATE('%m/%e/%Y', extract_date)
+                        ELSE PARSE_DATE('%F', extract_date)
+                    END DESC
+            ) AS _rownum,
+            CASE
+                WHEN extract_date LIKE '%/%' THEN PARSE_DATE('%m/%e/%Y', extract_date)
+                ELSE PARSE_DATE('%F', extract_date)
+            END AS _activities_on,
+            _6sense_company_name,
+            _6sense_country,
+            _6sense_domain,
+            CONCAT(
+                _6sense_company_name,
+                _6sense_country,
+                _6sense_domain
+            ) AS _country_account,
+            '6sense' AS _data_source,
+            buying_stage__start AS _previous_stage,
+            buying_stage__end AS _current_stage
+        FROM
+            `smartcom_6sense.buying_stage`
+    ),
+    latest_sixsense_buying_stage_with_order_and_movement AS (
+        SELECT main.*
+            EXCEPT (_rownum),
+            prev._order AS _previous_stage_order,
+            curr._order AS _current_stage_order,
+                CASE
+                    WHEN curr._order > prev._order THEN '+ve'
+                    WHEN prev._order > curr._order THEN '-ve'
+                    ELSE 'Stagnant'
+                END AS _movement
+        FROM
+            sixsense_buying_stage_data AS main
+            LEFT JOIN sixsense_stage_order AS prev
+                ON main._previous_stage = prev._buying_stage
+            LEFT JOIN sixsense_stage_order AS curr
+                ON main._current_stage = curr._buying_stage
+        WHERE main._rownum = 1
+    )
+SELECT *
+FROM
+    latest_sixsense_buying_stage_with_order_and_movement;
+
+----------------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------
+-- ADS PERFORMANCES
+----------------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------
+
+
 CREATE OR REPLACE TABLE `smartcom.db_6sense_ad_performance` AS
 
 WITH ads AS (
