ce7ddfbefff1eee59d486066133d74349dd556f8
raaayden <46622202+raaayden@users.noreply.github.com>
2024-07-24 11:34:22 +0800

    Update faro_salesforce_log.sql

--- Clients/FARO/Analytics Script/faro_salesforce_log.sql
+++ Clients/FARO/Analytics Script/faro_salesforce_log.sql
@@ -165,42 +165,42 @@ ON campaignMembers.campaignid = campaigns.id;
 CREATE OR REPLACE TABLE `x-marketing.faro.db_activity_log` AS
 WITH activity AS (
   SELECT DISTINCT
-    id AS _activityID,
-    createddate AS _activityDate,
+    id AS _activity_id,
+    createddate AS _activity_date,
     description AS _description,
-    contact_id__c AS _contactID_C,
+    contact_id__c AS _contact_id_c,
     subject AS _subject,
     event_status__c AS _status,
-    type AS _activityType,
-    'Event' AS _activityCategory
+    type AS _activity_type,
+    'Event' AS _activity_category
   FROM `x-marketing.faro_salesforce.Event`
   UNION ALL
   SELECT DISTINCT
-    id AS _activityID,
-    createddate AS _activityDate,
+    id AS _activity_id,
+    createddate AS _activity_date,
     description AS _description,
-    contact_id__c AS _contactID_C,
+    contact_id__c AS _contact_id_c,
     subject AS _subject,
     event_status__c AS _status,
-    type AS _activityType,
-    'Task' AS _activityCategory
+    type AS _activity_type,
+    'Task' AS _activity_category
   FROM `x-marketing.faro_salesforce.Task`
 )
 SELECT * FROM activity
-WHERE EXTRACT(YEAR FROM _activityDate) > 2021;
+WHERE EXTRACT(YEAR FROM _activity_date) > 2021;
 
 
 CREATE OR REPLACE TABLE `x-marketing.faro.db_velocity_log` AS
 WITH velocity_data AS (
   SELECT
-    leads.leadid AS _prospectID,
+    leads.leadid AS _prospect_id,
     -- ld.name AS _prospectName,
     -- act.name AS _prospectOwner,
-    leads.oldvalue AS _oldValue,
-    leads.newvalue AS _newValue,
-    leads.createddate AS _createdDate,
+    leads.oldvalue AS _old_value,
+    leads.newvalue AS _new_value,
+    leads.createddate AS _created_date,
     leads.field AS _field,
-    'Leads' AS _prospectType
+    'Leads' AS _prospect_type
   FROM `x-marketing.faro_salesforce.LeadHistory` leads
   -- LEFT JOIN `x-marketing.faro_salesforce.Lead` ld ON leads.leadid = ld.id
   -- LEFT JOIN `x-marketing.faro_salesforce.Account` act ON ld.convertedaccountid = act.id
@@ -213,14 +213,14 @@ WITH velocity_data AS (
   -- QUALIFY ROW_NUMBER() OVER(PARTITION BY leads.id ORDER BY leads.createddate) = 1
   UNION ALL
   SELECT
-    contacts.contactid AS _prospectID,
+    contacts.contactid AS _prospect_id,
     -- ct.name AS _prospectName,
     -- mbr.name AS _prospectOwner,
-    contacts.oldvalue AS _oldValue,
-    contacts.newvalue AS _newValue,
-    contacts.createddate AS _createdDate,
+    contacts.oldvalue AS _old_value,
+    contacts.newvalue AS _new_value,
+    contacts.createddate AS _created_date,
     contacts.field AS _field,
-    'Contacts' AS _prospectType
+    'Contacts' AS _prospect_type
   FROM `x-marketing.faro_salesforce.ContactHistory` contacts
   -- LEFT JOIN `x-marketing.faro_salesforce.Contact` ct ON contacts.contactid = ct.id
   -- LEFT JOIN `x-marketing.faro_salesforce.CampaignMember` mbr ON ct.ownerid = mbr.leadorcontactownerid
@@ -235,84 +235,80 @@ WITH velocity_data AS (
 stages_data AS (
   SELECT
     velocity_data.*,
-    LEFT(velocity_data._prospectID,15) AS _prospectID15,
-    LEAD(velocity_data._createdDate) OVER (PARTITION BY velocity_data._prospectID ORDER BY velocity_data._createdDate) AS _next_change_date,
+    LEFT(velocity_data._prospect_id,15) AS _prospect_id15,
+    LEAD(velocity_data._created_date) OVER (PARTITION BY velocity_data._prospect_id ORDER BY velocity_data._created_date) AS _next_change_date,
     CASE
-      WHEN _oldValue = 'Inquiry' AND _newValue = 'Automated Qualified Lead' THEN 'AQL'
-      WHEN _oldValue IS NULL AND _newValue = 'Inquiry' THEN 'Inquiry'
-      WHEN _oldValue = 'Automated Qualified Lead' AND _newValue = 'Inside Sales Accepted Lead' THEN 'ISAL'
-      WHEN _oldValue = 'Automated Qualified Lead' AND _newValue = 'Sales Accepted Lead' THEN 'SAL'
-      WHEN _oldValue = 'Inside Sales Accepted Lead' AND _newValue = 'Inside Sales Qualified Lead' THEN 'ISQL'
-      WHEN _oldValue IS NULL AND _newValue = 'Inside Sales Generated Lead' THEN 'ISGL'
-      WHEN _oldValue = 'Inside Sales Qualified Lead' AND _newValue = 'Sales Accepted Lead' THEN 'SAL'
-      WHEN _oldValue = 'Inside Sales Generated Lead' AND _newValue = 'Sales Accepted Lead' THEN 'SAL'
-      WHEN _oldValue = 'Sales Generated Lead' AND _newValue = 'Sales Qualified Lead' THEN 'SQL'
-      WHEN _oldValue = 'Sales Accepted Lead' AND _newValue = 'Sales Qualified Lead' THEN 'SQL'
-      WHEN _newValue = 'No Current Interest-Recycled' THEN 'NCIR'
-      WHEN _newValue = 'Closed Won Opportunity' THEN 'Closed Won Opportunity'
-      WHEN _newValue = 'Closed Lost Opportunity' THEN 'Closed Lost Opportunity'
-      WHEN _newValue = 'Sales Rejected Lead' THEN 'Rejected'
-      WHEN _newValue = 'Inside Sales Rejected Lead' THEN 'Rejected'
+      WHEN _old_value = 'Inquiry' AND _new_value = 'Automated Qualified Lead' THEN 'AQL'
+      WHEN _old_value IS NULL AND _new_value = 'Inquiry' THEN 'Inquiry'
+      WHEN _old_value = 'Automated Qualified Lead' AND _new_value = 'Inside Sales Accepted Lead' THEN 'ISAL'
+      WHEN _old_value = 'Automated Qualified Lead' AND _new_value = 'Sales Accepted Lead' THEN 'SAL'
+      WHEN _old_value = 'Inside Sales Accepted Lead' AND _new_value = 'Inside Sales Qualified Lead' THEN 'ISQL'
+      WHEN _old_value IS NULL AND _new_value = 'Inside Sales Generated Lead' THEN 'ISGL'
+      WHEN _old_value = 'Inside Sales Qualified Lead' AND _new_value = 'Sales Accepted Lead' THEN 'SAL'
+      WHEN _old_value = 'Inside Sales Generated Lead' AND _new_value = 'Sales Accepted Lead' THEN 'SAL'
+      WHEN _old_value = 'Sales Generated Lead' AND _new_value = 'Sales Qualified Lead' THEN 'SQL'
+      WHEN _old_value = 'Sales Accepted Lead' AND _new_value = 'Sales Qualified Lead' THEN 'SQL'
+      WHEN _new_value = 'No Current Interest-Recycled' THEN 'NCIR'
+      WHEN _new_value = 'Closed Won Opportunity' THEN 'Closed Won Opportunity'
+      WHEN _new_value = 'Closed Lost Opportunity' THEN 'Closed Lost Opportunity'
+      WHEN _new_value = 'Sales Rejected Lead' THEN 'Rejected'
+      WHEN _new_value = 'Inside Sales Rejected Lead' THEN 'Rejected'
       ELSE 'Skipped Stage'
     END AS _2x_stages,
-    CONCAT(_oldValue,_newValue) AS _old_new
+    CONCAT(_old_value,_new_value) AS _old_new
   FROM velocity_data
 ),
 activity_data AS (
   SELECT DISTINCT
-    _contactID_C AS _prospectID,
+    _contact_id_c AS _prospect_id,
     _subject,
-    _activityDate
+    _activity_date
   FROM `x-marketing.faro.db_activity_log` 
-  ORDER BY _activityDate
+  ORDER BY _activity_date
 ),
 activity AS (
   SELECT
-    stages_data._prospectID,
+    stages_data._prospect_id,
     stages_data._old_new,
-    stages_data._createdDate,
+    stages_data._created_date,
     stages_data._next_change_date,
     COUNT(activity_data._subject) AS _activity_count
   FROM 
-    stages_data 
+    stages_data
   LEFT JOIN 
     activity_data
   ON 
-    activity_data._prospectID = stages_data._prospectID15
-    AND activity_data._activityDate 
-    BETWEEN stages_data._createdDate AND stages_data._next_change_date
+    activity_data._prospect_id = stages_data._prospect_id15
+    AND activity_data._activity_date 
+    BETWEEN stages_data._created_date AND stages_data._next_change_date
   GROUP BY 
-    stages_data._prospectID,
+    stages_data._prospect_id,
     stages_data._old_new,
-    stages_data._createdDate,
+    stages_data._created_date,
     stages_data._next_change_date
 )
-SELECT
-  stages_data._prospectID,
-  stages_data._oldValue,
-  stages_data._newValue,
-  stages_data._createdDate,
+SELECT DISTINCT
+  stages_data._prospect_id,
+  stages_data._old_value,
+  stages_data._new_value,
+  stages_data._created_date,
+  stages_data._next_change_date,
   stages_data._field,
-  stages_data._prospectType,
+  stages_data._prospect_type,
   stages_data._2x_stages,
-  COALESCE(
-    DATE_DIFF(
-      DATE(stages_data._createdDate), 
-      LAG(DATE(stages_data._createdDate)) OVER (PARTITION BY stages_data._prospectID 
-      ORDER BY DATE(stages_data._createdDate)), DAY),
+  IF(
+    stages_data._next_change_date IS NULL,
+    COALESCE( 
+      DATE_DIFF(CURRENT_DATE(), DATE(stages_data._created_date),DAY),
       0
-  ) AS stage_change_duration_days,
+    ),
+    COALESCE(
+      DATE_DIFF(DATE(stages_data._next_change_date),DATE(stages_data._created_date),DAY),
+      0
+    )
+  ) AS _stage_change_duration_days,
   activity._activity_count
 FROM stages_data
 LEFT JOIN activity
-ON activity._prospectID = stages_data._prospectID
-AND activity._old_new = CONCAT(stages_data._oldValue,stages_data._newValue);
-
-
-
-
-
-
-
-
-
+ON activity._prospect_id = stages_data._prospect_id
+AND activity._old_new = CONCAT(stages_data._old_value,stages_data._new_value);
