ce8c43164eb54f91495d751915d475a8bc8241bb
nazzatul21 <Nazzatul.Nazziri@2x.marketing>
2024-08-14 16:56:50 +0800

    update the request change

--- Clients/Jellyvision/facebook_ads_performance.sql
+++ Clients/Jellyvision/facebook_ads_performance.sql
@@ -49,7 +49,7 @@ WITH FB_ads AS (
         lifetime_budget AS _lifetime_budget,
         budget_remaining AS _budget_remaining, 
         end_time AS _end_time 
-    FROM `x-marketing.jellyvision_facebook_ads.adsets`
+  FROM `x-marketing.jellyvision_facebook_ads.adsets`
 )
 , ad_campaign AS (
     SELECT 
@@ -81,59 +81,52 @@ WITH FB_ads AS (
 )
 , combine_all AS (
     SELECT 
-        * EXCEPT (rownum)
-    FROM (
-            SELECT 
-                FB_ads.*,
-                ad_names._bid_type, 
-                ad_names._status_ads, 
-                ad_names._body, 
-                ad_names._title, 
-                --ad_names.url_tags, 
-                ad_names._status_creative, 
-                ad_names._creative_name, 
-                ad_names._thumbnail_url, 
-                ad_names._instagram_actor_id, 
-                ad_names._image_url,
-                ad_adsets._created_time, 
-                ad_adsets._lifetime_budget,
-                ad_adsets._budget_remaining, 
-                ad_adsets._end_time,
-                ad_campaign._campaign_objective, 
-                ad_campaign._buying_type, 
-                ad_campaign._effective_status, 
-                ad_campaign._updated_time,
-                CAST(ad_names._adid AS INT64) AS _creative_id,
-                airtable_ads.* EXCEPT (_adid),
-                ROW_NUMBER() OVER(
-                    PARTITION BY 
-                        FB_ads._adid,
-                        FB_ads._adset_id,
-                        FB_ads._campaign_id,
-                        FB_ads._ads_name,
-                        FB_ads._adset_name,
-                        FB_ads._campaign_name,
-                        FB_ads._date_start,
-                        FB_ads._reach,
-                        FB_ads._impressions,
-                        FB_ads._clicks,
-                        FB_ads._inline_link_clicks,
-                        FB_ads._unique_inline_link_clicks
-                        
-                        --CAST(conversions AS INT64)
-                    ORDER BY FB_ads._date_start, CAST(FB_ads._conversions AS INT64)  DESC) AS rownum
-            FROM FB_ads
-            LEFT JOIN ad_names 
-                ON ad_names._adid = FB_ads._adid
-            LEFT JOIN ad_adsets 
-                ON ad_adsets._adset_id = FB_ads._adset_id
-            LEFT JOIN ad_campaign 
-                ON ad_campaign._campaign_id = FB_ads._campaign_id
-            LEFT JOIN airtable_ads 
-                ON FB_ads._ads_name = airtable_ads._advariation 
-                OR FB_ads._adid = CAST(airtable_ads._adid AS STRING)
-        ) 
-        WHERE rownum = 1 
+        FB_ads.*,
+        ad_names._bid_type, 
+        ad_names._status_ads, 
+        ad_names._body, 
+        ad_names._title, 
+        --ad_names.url_tags, 
+        ad_names._status_creative, 
+        ad_names._creative_name, 
+        ad_names._thumbnail_url, 
+        ad_names._instagram_actor_id, 
+        ad_names._image_url,
+        ad_adsets._created_time, 
+        ad_adsets._lifetime_budget,
+        ad_adsets._budget_remaining, 
+        ad_adsets._end_time,
+        ad_campaign._campaign_objective, 
+        ad_campaign._buying_type, 
+        ad_campaign._effective_status, 
+        ad_campaign._updated_time,
+        CAST(ad_names._adid AS INT64) AS _creative_id,
+        airtable_ads.* EXCEPT (_adid),
+    FROM FB_ads
+    LEFT JOIN ad_names 
+        ON ad_names._adid = FB_ads._adid
+    LEFT JOIN ad_adsets 
+        ON ad_adsets._adset_id = FB_ads._adset_id
+    LEFT JOIN ad_campaign 
+        ON ad_campaign._campaign_id = FB_ads._campaign_id
+    LEFT JOIN airtable_ads 
+        ON FB_ads._ads_name = airtable_ads._advariation 
+        OR FB_ads._adid = CAST(airtable_ads._adid AS STRING) 
+    QUALIFY ROW_NUMBER() 
+        OVER( PARTITION BY 
+        FB_ads._adid,
+        FB_ads._adset_id,
+        FB_ads._campaign_id,
+        FB_ads._ads_name,
+        FB_ads._adset_name,
+        FB_ads._campaign_name,
+        FB_ads._date_start,
+        FB_ads._reach,
+        FB_ads._impressions,
+        FB_ads._clicks,
+        FB_ads._inline_link_clicks,
+        FB_ads._unique_inline_link_clicks
+    ORDER BY FB_ads._date_start, CAST(FB_ads._conversions AS INT64)  DESC) = 1 
 )
 , total_ads_per_campaign AS (
     SELECT
@@ -146,14 +139,8 @@ WITH FB_ads AS (
 , daily_budget_per_ad_per_campaign AS (
     SELECT
         *,
-        CASE WHEN _ads_per_campaign > 0 THEN _lifetime_budget / _ads_per_campaign 
-        ELSE 0
-        END
-           AS _dailyBudget_per_ad,
-          CASE WHEN _ads_per_campaign > 0 THEN _budget_remaining / _ads_per_campaign 
-        ELSE 0
-        END
-        AS _dailybudget_remaining_per_ad
+        IF (_ads_per_campaign > 0, _lifetime_budget / _ads_per_campaign, 0) AS _dailyBudget_per_ad,
+        IF ( _ads_per_campaign > 0, _budget_remaining / _ads_per_campaign, 0) AS _dailybudget_remaining_per_ad
 
     FROM total_ads_per_campaign
 )
