e40fa2bc11fbbec191a067a2dfb74113d41d11bd
azwansyafiq-2X <azwan.syafiq@2x.marketing>
2024-10-07 15:56:06 +0800

    Use DML instead of DDL Use a proper column name

--- Clients/Jellyvision/db_6sense_account.sql
+++ Clients/Jellyvision/db_6sense_account.sql
@@ -2,27 +2,42 @@
 -----------------------------------------------------------------------------------------------
 -----------------------------------------------------------------------------------------------
 
-----6Sense account
+---- 6Sense Account ----
+TRUNCATE TABLE `jellyvision.db_6sense_account`;
 
-
-CREATE OR REPLACE TABLE `jellyvision.db_6sense_account` AS
+INSERT INTO `jellyvision.db_6sense_account` (
+  _is_6qa,
+  _account_profile_score_6sense,
+  _account_6qa_age_in_days_6sense,
+  _account_reach_score_6sense,
+  _account_intent_score_6sense,
+  _account_buying_stage_6sense, 
+  _account_profile_fit_6sense, 
+  _account_6qa_end_date_6sense,
+  _account_update_date_6sense,
+  _account_numerical_reach_score_6sense, 
+  _account_6qa_start_date_6sense,
+  _account_name,
+  _domain,
+  _revenue,
+  _industry
+)
 
 SELECT 
     account6qa6sense__c AS  _is_6qa,
-    accountprofilescore6sense__c AS _accountprofilescore6sense, 
-    account6qaageindays6sense__c AS _account6qaageindays6sense, 
-    accountreachscore6sense__c AS _accountreachscore6sense, 
-    accountintentscore6sense__c AS _accountintentscore6sense, 
-    accountbuyingstage6sense__c AS _accountbuyingstage6sense, 
-    accountprofilefit6sense__c AS _accountprofilefit6sense, 
-    account6qaenddate6sense__c AS _account6qaenddate6sense,
-    accountupdatedate6sense__c AS accountupdatedate6sense__c,
-    accountnumericalreachscore6sense__c AS _accountnumericalreachscore6sense, 
-    account6qastartdate6sense__c AS _account6qastartdate6sense,
+    accountprofilescore6sense__c AS _account_profile_score_6sense, 
+    account6qaageindays6sense__c AS _account_6qa_age_in_days_6sense, 
+    accountreachscore6sense__c AS _account_reach_score_6sense, 
+    accountintentscore6sense__c AS _account_intent_score_6sense, 
+    accountbuyingstage6sense__c AS _account_buying_stage_6sense, 
+    accountprofilefit6sense__c AS _account_profile_fit_6sense, 
+    account6qaenddate6sense__c AS _account_6qa_end_date_6sense,
+    accountupdatedate6sense__c AS account_update_date_6sense,
+    accountnumericalreachscore6sense__c AS _account_numerical_reach_score_6sense, 
+    account6qastartdate6sense__c AS _account_6qa_start_date_6sense,
     name AS _account_name,
     web_domain_name__c  AS _domain,
     jv_revenue_estimate__c AS _revenue,
     industry_variable__c AS _industry
 FROM `x-marketing.jellyvision_salesforce.Account`
-
 WHERE isdeleted IS FALSE;
