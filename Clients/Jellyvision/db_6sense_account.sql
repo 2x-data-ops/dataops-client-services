af6a857947ceb29dd3074dc6a7e6a66be37f8a37
azwansyafiq-2X <azwan.syafiq@2x.marketing>
2024-10-09 14:56:18 +0800

    Adding missing underscore and adjust the indent size

--- Clients/Jellyvision/db_6sense_account.sql
+++ Clients/Jellyvision/db_6sense_account.sql
@@ -24,20 +24,20 @@ INSERT INTO `jellyvision.db_6sense_account` (
 )
 
 SELECT 
-    account6qa6sense__c AS  _is_6qa,
-    accountprofilescore6sense__c AS _account_profile_score_6sense, 
-    account6qaageindays6sense__c AS _account_6qa_age_in_days_6sense, 
-    accountreachscore6sense__c AS _account_reach_score_6sense, 
-    accountintentscore6sense__c AS _account_intent_score_6sense, 
-    accountbuyingstage6sense__c AS _account_buying_stage_6sense, 
-    accountprofilefit6sense__c AS _account_profile_fit_6sense, 
-    account6qaenddate6sense__c AS _account_6qa_end_date_6sense,
-    accountupdatedate6sense__c AS account_update_date_6sense,
-    accountnumericalreachscore6sense__c AS _account_numerical_reach_score_6sense, 
-    account6qastartdate6sense__c AS _account_6qa_start_date_6sense,
-    name AS _account_name,
-    web_domain_name__c  AS _domain,
-    jv_revenue_estimate__c AS _revenue,
-    industry_variable__c AS _industry
+  account6qa6sense__c AS  _is_6qa,
+  accountprofilescore6sense__c AS _account_profile_score_6sense, 
+  account6qaageindays6sense__c AS _account_6qa_age_in_days_6sense, 
+  accountreachscore6sense__c AS _account_reach_score_6sense, 
+  accountintentscore6sense__c AS _account_intent_score_6sense, 
+  accountbuyingstage6sense__c AS _account_buying_stage_6sense, 
+  accountprofilefit6sense__c AS _account_profile_fit_6sense, 
+  account6qaenddate6sense__c AS _account_6qa_end_date_6sense,
+  accountupdatedate6sense__c AS _account_update_date_6sense,
+  accountnumericalreachscore6sense__c AS _account_numerical_reach_score_6sense, 
+  account6qastartdate6sense__c AS _account_6qa_start_date_6sense,
+  name AS _account_name,
+  web_domain_name__c  AS _domain,
+  jv_revenue_estimate__c AS _revenue,
+  industry_variable__c AS _industry
 FROM `x-marketing.jellyvision_salesforce.Account`
 WHERE isdeleted IS FALSE;
